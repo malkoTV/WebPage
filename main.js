6273f1864b150d2ec236377ea3fac46bebf89992
Tamara Malko <73736618+malkoTV@users.noreply.github.com>
2020-11-23 17:49:36 +0200

    basic structure

--- main.js
+++ main.js
@@ -1,21 +1,19 @@
-<script>
-function myFunction() {
-  var dots = document.getElementById("dots");
-  var moreText = document.getElementById("more");
-  var btnText = document.getElementById("myBtn");
-
-  if (dots.style.display === "none")
-	{
+function readMoreOrLess(dotsS, moreTextS, btnTextS) {
+	var dots = document.getElementById(dotsS);
+	var moreText = document.getElementById(moreTextS);
+	var btnText = document.getElementById(btnTextS);
+  if (dots.style.display === "none") {
     dots.style.display = "inline";
     btnText.innerHTML = "Read more";
     moreText.style.display = "none";
-  }
-	else
-	{
+  } else {
     dots.style.display = "none";
     btnText.innerHTML = "Read less";
     moreText.style.display = "inline";
   }
 }
 
-</script>
+function foo(name)
+{
+  document.write("Hello, I am " + name);
+}
